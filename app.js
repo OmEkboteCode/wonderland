3c31383f65b1bec050c5dd07ce8431d06741d751
OmEkboteCode <omekbote007@gmail.com>
2026-08-31 20:43:01 +0530

    Add New And Create Route

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ exotended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Working");
+  new Listing();
 });
 
 // Index route
@@ -33,12 +34,25 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 // Show Route
 
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+// Create Route
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 app.listen(8080, () => {
