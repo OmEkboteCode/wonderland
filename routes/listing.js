62879f9e9ea7b57adf5511b24afbe94b5d7d6d29
OmEkboteCode <omekbote007@gmail.com>
2026-09-22 20:39:58 +0530

    Implement Flash Failures

--- routes/listing.js
+++ routes/listing.js
@@ -37,6 +37,10 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+      req.flash("error", "Listing You Requested For Does Not Exist");
+      return res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -49,7 +53,7 @@ router.post(
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    req.flash("success", "New Listing Created!")
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
   }),
 );
@@ -62,6 +66,10 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+      req.flash("error", "Listing You Requested For Does Not Exist");
+      return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
   }),
 );
@@ -73,7 +81,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("success", "Listing Updated!")
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   }),
 );
@@ -86,7 +94,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
-    req.flash("success", "Listing Deleted!")
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   }),
 );
