bb8c1444a4e24a9b3f7fc3b06c41805c7471fb6d
OmEkboteCode <omekbote007@gmail.com>
2026-09-26 13:26:58 +0530

    Implement Login Route

--- routes/user.js
+++ routes/user.js
@@ -2,24 +2,43 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req, res) => {
-    try{
-  let { username, email, password } = req.body;
-  const newUser = new User({ email, username });
-  const registeredUser = await User.register(newUser, password);
-  console.log(registeredUser);
-  req.flash("Success", "Welcome to Wonderland");
-  res.redirect("/listings")
-    } catch(err) {
-        req.flash("error", err.message)
-        res.redirect("/signup")
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Wonderland");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
     }
+  }),
+);
 
-}));
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome Back To WonderLand!");
+    res.redirect("/listings");
+  },
+);
 
 module.exports = router;
